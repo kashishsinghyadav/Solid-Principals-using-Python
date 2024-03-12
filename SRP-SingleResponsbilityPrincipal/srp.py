06ced724e9a14149d3a47d4c5b05e95fcf9f224e
kashishsinghyadav <kashishhsinghhh@gmail.com>
2024-03-12 10:41:56 +0530

    srp

---
+++ SRP-SingleResponsbilityPrincipal/srp.py
@@ -0,0 +1,11 @@
+class Authentication:
+    
+    def __init__(self,name,rollno,password):
+        self.name=name
+        self.rollno=rollno
+        self.password = password
+    def cheack(self,name,rollno):
+        pass 
+    def authuser(self,name,password):
+        pass 
+    
